b2b42389cc3547f976705bcd8123082abdd51304
Troy <hello@troylusty.com>
2025-01-18 21:49:08 +0000

    build site using bun

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,29 @@ import rehypeExternalLinks from "rehype-external-links";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://troylusty.com",
-  integrations: [sitemap(), mdx(), tailwind(), icon()],
+  integrations: [
+    sitemap(),
+    expressiveCode({
+      themes: ["github-dark-default", "github-light-default"],
+      defaultProps: {
+        frame: "none",
+        wrap: true,
+      },
+      styleOverrides: {
+        frames: {
+          shadowColor: "none",
+        },
+      },
+    }),
+    mdx(),
+    tailwind(),
+    icon(),
+  ],
   output: "static",
   markdown: {
     rehypePlugins: [
@@ -19,10 +37,6 @@ export default defineConfig({
         },
       ],
     ],
-    shikiConfig: {
-      wrap: true,
-    },
-    syntaxHighlight: false,
   },
   image: {
     service: passthroughImageService(),
